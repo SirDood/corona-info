c016e901830bbc3096d9a36e16495c2869bb7cf5
Izzat Z <izzat.zainir11@gmail.com>
2022-05-09 00:04:50 +0800

    Created setup_unix.sh to easily setup the project on Unix/Linux systems

---
+++ setup_unix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+printf "Creating a virtual environment...\n"
+python3 -m venv corona
+printf "Virtual environment created.\n"
+source corona/bin/activate
+printf "\nInstalling dependencies...\n"
+pip install requests beautifulsoup4
+printf "Dependencies installed.\n"
