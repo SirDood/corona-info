e877bd2f138ca8f44de55220df2f0bd7e8ddac7f
Izzat Z <izzat.zainir11@gmail.com>
2022-06-14 23:34:18 +0800

    Modified the setup script to use 'venv' as the name of the virtual environment

--- setup_unix.sh
+++ setup_unix.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 printf "Creating a virtual environment...\n"
-python3 -m venv corona
+python3 -m venv venv
 printf "Virtual environment created.\n"
-source corona/bin/activate
+source venv/bin/activate
 printf "\nInstalling dependencies...\n"
 pip install requests beautifulsoup4
 printf "Dependencies installed.\n"
 
-printf "\nDon't forget to run 'source corona/bin/activate' before running the script!\n"
+printf "\nDon't forget to run 'source venv/bin/activate' before running the script!\n"
