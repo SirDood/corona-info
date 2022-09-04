31f9bfa8db35aeb1b646b8cff46a0867ba3bfd31
Izzat Z <izzat.zainir11@gmail.com>
2022-09-04 18:30:18 +0800

    Add function in main to run the GUI properly

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 import sys
 
 from coronainfo.models import CoronaModel
@@ -11,6 +13,31 @@ def main(*args):
         print(country)
 
 
+def main_ui():
+    import gi
+    from gi.repository import Gio
+
+    SOURCE_DIR = os.path.join(os.path.dirname(__file__), "coronainfo")
+    GRESOURCE_XML = os.path.join(SOURCE_DIR, "coronainfo.gresource.xml")
+    GRESOURCE_BIN = os.path.join(SOURCE_DIR, "resources", "gresource")
+
+    if not os.path.exists(GRESOURCE_BIN):
+        command = [
+            "glib-compile-resources",
+            f"--sourcedir={SOURCE_DIR}",
+            f"--target={GRESOURCE_BIN}",
+            GRESOURCE_XML
+        ]
+        subprocess.call(command)
+
+    resource = Gio.Resource.load(GRESOURCE_BIN)
+    resource._register()
+
+    from coronainfo import app
+    sys.exit(app.main("0.1.0"))
+
+
 if __name__ == "__main__":
     sys_args = sys.argv[1:]
-    main(*sys_args)
+    # main(*sys_args)
+    main_ui()
