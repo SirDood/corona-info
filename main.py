5a9c6ff646d3f64655ccf21c8ef91ce922d07e2a
Izzat Z <izzat.zainir11@gmail.com>
2022-09-10 23:33:53 +0800

    Minor tweaks to main.py

--- main.py
+++ main.py
@@ -10,29 +10,29 @@ GRESOURCE_BIN = SOURCE_DIR / "resources" / "gresource"
 
 SCICONS_SRC = SOURCE_DIR.parent / "data" / "icons" / "hicolor" / "scalable" / "apps"
 SCICONS_DEST = Path.home() / ".icons" / "hicolor" / "scalable" / "apps"
-if not SCICONS_DEST.exists():
-    SCICONS_DEST.mkdir(parents=True, exist_ok=True)
+SCICONS_DEST.mkdir(parents=True, exist_ok=True)
 
 SYICONS_SRC = SOURCE_DIR.parent / "data" / "icons" / "hicolor" / "symbolic" / "apps"
 SYICONS_DEST = Path.home() / ".icons" / "hicolor" / "symbolic" / "apps"
-if not SYICONS_DEST.exists():
-    SYICONS_DEST.mkdir(parents=True, exist_ok=True)
-
-ICONS_LIST: list[Path] = []
+SYICONS_DEST.mkdir(parents=True, exist_ok=True)
 
 
 def install_icons(source: Path, destination: Path):
+    icons_list = []
     for file in source.iterdir():
         if file.is_file() and file.suffix == ".svg":
             dest = destination / file.name
             print(f"Installing: {file} -> {dest}")
-            ICONS_LIST.append(dest)
+            icons_list.append(dest)
             shutil.copy(file, destination)
+
+    return icons_list
 
 
 def main():
     from gi.repository import Gio
 
+    # Compile resources
     command = [
         "glib-compile-resources",
         f"--sourcedir={SOURCE_DIR}",
@@ -41,17 +41,20 @@ def main():
     ]
     subprocess.call(command)
 
-    install_icons(SCICONS_SRC, SCICONS_DEST)
-    install_icons(SYICONS_SRC, SYICONS_DEST)
+    # Install icons
+    installed_icons = install_icons(SCICONS_SRC, SCICONS_DEST) + install_icons(SYICONS_SRC, SYICONS_DEST)
 
+    # Load resources
     resource = Gio.Resource.load(str(GRESOURCE_BIN))
     resource._register()
 
+    # Run app
     from coronainfo import app
     from coronainfo.enums import App
     code = app.main(App.VERSION)
 
-    for icon in ICONS_LIST:
+    # Delete icons after app quits
+    for icon in installed_icons:
         print(f"Deleting:", icon)
         os.remove(icon)
 
