280d678bc3e11705f68dac43d531b876d9ce1234
Izzat Z <izzat.zainir11@gmail.com>
2022-09-14 17:56:43 +0800

    Refactored MainWindow

--- coronainfo/views/window_main.py
+++ coronainfo/views/window_main.py
@@ -28,42 +28,37 @@ class MainWindow(Adw.ApplicationWindow):
     __gtype_name__ = "MainWindow"
 
     refresh_btn: Gtk.Button = Gtk.Template.Child(name="refresh_btn")
-    spinner: Gtk.Spinner = Gtk.Template.Child(name="spinner")
     search_btn: Gtk.ToggleButton = Gtk.Template.Child(name="search_btn")
+
+    content_box: Gtk.Box = Gtk.Template.Child(name="content_box")
+
+    spinner_box: Gtk.Box = Gtk.Template.Child(name="spinner_box")
+    spinner: Gtk.Spinner = Gtk.Template.Child(name="spinner")
+
+    table_box: Gtk.Box = Gtk.Template.Child(name="table_box")
     searchbar: Gtk.SearchBar = Gtk.Template.Child(name="searchbar")
     search_entry: Gtk.SearchEntry = Gtk.Template.Child(name="search_entry")
-    content_box: Gtk.Box = Gtk.Template.Child(name="content_box")
     table: Gtk.TreeView = Gtk.Template.Child(name="table_view")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._init_settings()
-
-        title = evaluate_title(app.get_settings())
-        self.set_title(title)
-
-        # Set the shortcuts window aka help overlay
-        builder: Gtk.Builder = Gtk.Builder.new_from_resource("/coronainfo/ui/help-overlay")
-        shortcuts_window: Gtk.ShortcutsWindow = builder.get_object("help_overlay")
-        self.set_help_overlay(shortcuts_window)
+        self._setup_help_overlay()
+        self._bind_properties()
 
         create_action(self, "refresh-data", self.on_refresh_action, ["<Ctrl>r"])
         create_action(self, "save-data", self.on_save_action, ["<Ctrl>s"])
         create_action(self, "preferences", self.on_preferences_action, ["<Ctrl>comma"])
         create_action(self, "toggle-search", self.on_toggle_search_action)
 
+        title = evaluate_title(app.get_settings())
+        self.set_title(title)
+
         self.searchbar.connect_entry(self.search_entry)
         self.searchbar.set_key_capture_widget(self)
-        self.searchbar.bind_property(
-            "search-mode-enabled",
-            self.search_btn,
-            "active",
-            GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE
-        )
 
         self.controller = AppController.get_main_controller()
         self.controller.set_table(self.table)
-
         self.controller.connect(self.controller.POPULATE_STARTED, self.on_populate_started)
         self.controller.connect(self.controller.POPULATE_FINISHED, self.on_populate_finished)
         self.controller.connect(self.controller.PROGRESS_MESSAGE, self.on_progress_emitted)
@@ -71,18 +66,12 @@ class MainWindow(Adw.ApplicationWindow):
 
     def on_populate_started(self, controller):
         self.refresh_btn.set_sensitive(False)
-        self.searchbar.set_visible(False)
-        self.table.set_visible(False)
-        self.content_box.set_valign(Gtk.Align.CENTER)
-        self.spinner.set_visible(True)
+        self.spinner_box.set_visible(True)
         self.spinner.start()
 
     def on_populate_finished(self, controller):
         self.spinner.stop()
-        self.spinner.set_visible(False)
-        self.content_box.set_valign(Gtk.Align.FILL)
-        self.table.set_visible(True)
-        self.searchbar.set_visible(True)
+        self.spinner_box.set_visible(False)
         self.refresh_btn.set_sensitive(True)
 
     def on_progress_emitted(self, controller, message: str):
@@ -133,4 +122,24 @@ class MainWindow(Adw.ApplicationWindow):
             self,
             "maximized",
             Gio.SettingsBindFlags.DEFAULT
+        )
+
+    def _setup_help_overlay(self):
+        builder: Gtk.Builder = Gtk.Builder.new_from_resource("/coronainfo/ui/help-overlay")
+        shortcuts_window: Gtk.ShortcutsWindow = builder.get_object("help_overlay")
+        self.set_help_overlay(shortcuts_window)
+
+    def _bind_properties(self):
+        self.search_btn.bind_property(
+            "active",
+            self.searchbar,
+            "search-mode-enabled",
+            GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE
+        )
+
+        self.spinner_box.bind_property(
+            "visible",
+            self.table_box,
+            "visible",
+            GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.INVERT_BOOLEAN
         )
